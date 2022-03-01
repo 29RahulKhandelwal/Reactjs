5976601e05181d6d54c83a15559199cf7cb9950b
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-01 07:57:13 +0530

    let&const done

---
+++ 02 - JS Refresher/let & const.js
@@ -0,0 +1,18 @@
+var myName="Rahul";
+console.log(myName);
+
+myName="Rahul Khandelwal";
+console.log(myName);
+
+let myName1="Let";
+console.log(myName1);
+
+myName1="let keyword";
+console.log(myName1);
+
+const myName2="const";
+console.log(myName2);
+
+// myName2="const keyword";
+// console.log(myName2);
+
