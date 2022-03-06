522fbf7d33652dedd74909c8cc42b88bb3932506
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-06 20:40:05 +0530

    used map f()

--- 03 - React Basics and Working with components/expense-tracker/src/App.js
+++ 03 - React Basics and Working with components/expense-tracker/src/App.js
@@ -1,14 +1,17 @@
 import ExpenseItem from "./components/ExpenseItem"
 import expenses from "./components/expenses";
 
+function createExpenseItemCard(expenses){
+    return (
+        <ExpenseItem date={expenses.date.toISOString()} title={expenses.title} amount={expenses.amount} />
+    )
+}
+
 function App(){
     return (
         <div>
             <h2>Let's get started</h2>
-            <ExpenseItem date={expenses[0].date.toISOString()} title={expenses[0].title} amount={expenses[0].amount} />
-            <ExpenseItem date={expenses[1].date.toISOString()} title={expenses[1].title} amount={expenses[1].amount} />
-            <ExpenseItem date={expenses[2].date.toISOString()} title={expenses[2].title} amount={expenses[2].amount} />
-            <ExpenseItem date={expenses[3].date.toISOString()} title={expenses[3].title} amount={expenses[3].amount} />
+            {expenses.map(createExpenseItemCard)}
         </div>
     )
 }
