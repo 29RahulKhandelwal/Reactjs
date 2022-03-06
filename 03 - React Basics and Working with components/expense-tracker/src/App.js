17f6cefbb017c33137b2b8e78861131c4648dd23
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-06 20:33:30 +0530

    passed props as date,title,amount in ExpenseItem component

--- 03 - React Basics and Working with components/expense-tracker/src/App.js
+++ 03 - React Basics and Working with components/expense-tracker/src/App.js
@@ -1,11 +1,14 @@
 import ExpenseItem from "./components/ExpenseItem"
-
+import expenses from "./components/expenses";
 
 function App(){
     return (
         <div>
             <h2>Let's get started</h2>
-            <ExpenseItem />
+            <ExpenseItem date={expenses[0].date.toISOString()} title={expenses[0].title} amount={expenses[0].amount} />
+            <ExpenseItem date={expenses[1].date.toISOString()} title={expenses[1].title} amount={expenses[1].amount} />
+            <ExpenseItem date={expenses[2].date.toISOString()} title={expenses[2].title} amount={expenses[2].amount} />
+            <ExpenseItem date={expenses[3].date.toISOString()} title={expenses[3].title} amount={expenses[3].amount} />
         </div>
     )
 }
