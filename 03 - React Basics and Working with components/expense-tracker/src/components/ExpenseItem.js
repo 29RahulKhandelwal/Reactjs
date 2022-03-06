7f8035909ad82d36c1ac379200fde478b7e86348
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-06 20:12:28 +0530

    outputtting dynamic data & working with expressions in jsx

--- 03 - React Basics and Working with components/expense-tracker/src/components/ExpenseItem.js
+++ 03 - React Basics and Working with components/expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(){
+    const expenseDate=new Date(2022, 2, 6);
+    const expenseTitle="Car Insurance";
+    const expenseAmount=560.67;
     return (
     <div className="expense-item">
-        <div>March 28th 2022</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
+            <h2>{expenseTitle}</h2>
         </div>
-        <div className="expense-item__price">$560.55</div>
+        <div className="expense-item__price">${expenseAmount}</div>
     </div>
     )
 }
