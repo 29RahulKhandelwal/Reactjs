8e2ae78c776d38796b9209461b50268e4f12fe51
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-06 19:38:59 +0530

    added css to elements

--- 03 - React Basics and Working with components/expense-tracker/src/components/ExpenseItem.js
+++ 03 - React Basics and Working with components/expense-tracker/src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem(){
     return (
-    <div>
+    <div className="expense-item">
         <div>March 28th 2022</div>
-        <div>
+        <div className="expense-item__description">
             <h2>Car Insurance</h2>
         </div>
-        <div>$560.55</div>
+        <div className="expense-item__price">$560.55</div>
     </div>
     )
 }
