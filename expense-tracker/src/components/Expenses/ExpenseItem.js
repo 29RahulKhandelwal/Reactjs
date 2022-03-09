297aa7a852bae47e0e52dfa174c5f448d9712174
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-09 21:51:22 +0530

    used useState for handleclick

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,15 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props){
-    
+    const [heading,setHeading]=useState(props.title);
     function handleClick(){
-        console.log("Clicked !!!");
+        setHeading("Click!!")
     }
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{heading}</h2>
         </div>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={handleClick}>Change Title</button>
