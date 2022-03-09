6bf9fc705631f377c00097072fba1647d3678e43
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-09 21:46:23 +0530

    working with useState

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,10 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props){
+    
     function handleClick(){
         console.log("Clicked !!!");
     }
