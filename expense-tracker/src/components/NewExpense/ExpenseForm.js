3003dada16239067c795f4c3b7eb1e61233b2408
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-10 20:35:21 +0530

    added onSubmit handler for submitting expense data

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -16,19 +16,33 @@ export default function ExpenseForm(){
         setEnteredDate(event.target.value);
     }
 
-    return <form>
+    function submitHandler(event){
+        const expenseData={
+            title:entertedTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate)
+        }
+        console.log(expenseData)
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
+        
+        event.preventDefault();
+    }
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input onChange={titleChangeHandler} type="text" />
+                <input onChange={titleChangeHandler} value={entertedTitle} type="text" />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input onChange={amountChangeHandler} type="number" min="0.01" step="0.01" />
+                <input onChange={amountChangeHandler} value={enteredAmount} type="number" min="0.01" step="0.01" />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input onChange={dateChangeHandler} type="date" min="2022-01-01" max="2022-12-31" />
+                <input onChange={dateChangeHandler} value={enteredDate} type="date" min="2022-01-01" max="2022-12-31" />
             </div>
         </div>
         <div className="new-expense__actions">
