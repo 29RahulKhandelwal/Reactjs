ea3ec50822390c30101277c1c3650fd8ae8abbcd
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-11 17:12:07 +0530

    filter expenses according to year

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -11,12 +11,16 @@ function Expenses(props){
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
   return(
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-        {props.items.map((expense,index)=>(
+        {filteredExpenses.map((expense,index)=>(
           <ExpenseItem key={index} date={expense.date} title={expense.title} amount={expense.amount} />
         ))}
       </Card>
