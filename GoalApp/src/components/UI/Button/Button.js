d91c45f3c1c391497623555d7a2c41a1e2effcf6
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-14 18:45:00 +0530

    using CSS module for button styling

--- GoalApp/src/components/UI/Button/Button.js
+++ GoalApp/src/components/UI/Button/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Button.css"
+import styles from "./Button.module.css"
 
 // import styled from 'styled-components';
 
@@ -29,12 +29,12 @@ import styles from "./Button.css"
 //   }
 // `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
