9e4176be361de82aa8f8a588fef681c9a1088aa2
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-14 11:05:25 +0530

    setting inline style to input

--- GoalApp/src/components/CourseGoals/CourseInput/CourseInput.js
+++ GoalApp/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,13 +5,21 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid,setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
     setEnteredValue("")
   };
@@ -20,8 +28,8 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} style={{borderColor: !isValid ? 'red' : '',  backgroundColor: !isValid ? 'salmon' : ''}} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
