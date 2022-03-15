00e2f7215e637d2a81f0dd28c5dac246b3f8b3f3
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-15 12:50:44 +0530

    modified

--- 04-addUser/src/components/App.jsx
+++ 04-addUser/src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Button from "./Button";
 import Input from "./Input";
 
 export default function App(){
   return (
     <div>
-      <Input name="Username" />
-      <Input name="Age (Years)" />
-      <Button btnName="Add User" />
+      <Input name="Username " />
+      <Input name="Age (in years) " />
+      <button type="submit">Add User</button>
     </div>
   )
 }
--- 04-addUser/src/components/Input.jsx
+++ 04-addUser/src/components/Input.jsx
@@ -7,9 +7,6 @@ export default function Input(props){
             <label htmlFor="input">{props.name}</label>
             <input type="text" name="input" id="input" />
             </div>
-            <div>
-                <button type="submit">{props.btnName}</button>
-            </div>
         </div>
     )
 }
