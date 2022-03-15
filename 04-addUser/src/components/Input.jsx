17b5a33e57dcaefda4f1525ea0d0c5c11e011ddb
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-15 12:38:09 +0530

    input component created

---
+++ 04-addUser/src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input(){
+    return (
+        <div>
+            <label htmlFor="input">Username</label>
+            <input type="text" name="input" id="input" />
+        </div>
+    )
+}
