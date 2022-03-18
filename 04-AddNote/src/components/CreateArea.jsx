eef5fe81e650646e9fc958db057915cc8f3b4773
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-18 16:00:17 +0530

    AddNote Project Done

--- 04-AddNote/src/components/CreateArea.jsx
+++ 04-AddNote/src/components/CreateArea.jsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
+
+  function handleCancel() {
+    setExpanded(false);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={submitNote}>
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        {isExpanded && <button className="addNote" type="submit" >Add</button>}
+        {isExpanded && <button className="cancelNote" onClick={handleCancel}>Cancel</button>}
       </form>
     </div>
   );
