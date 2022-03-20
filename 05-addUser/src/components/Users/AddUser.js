9b83f66a835fcf7cc6f64960b159c72d4e19094b
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-20 11:08:05 +0530

    created AddUser.module.css

--- 05-addUser/src/components/Users/AddUser.js
+++ 05-addUser/src/components/Users/AddUser.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Card from "../UI/Card";
 
 export default function AddUser(props){
     function addUserHandler(event){
-        
+
         event.preventDefault();
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-            <label htmlFor="age">Age (in years)</label>
-            <input type="number" id="age" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
+                <label htmlFor="age">Age (in years)</label>
+                <input type="number" id="age" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     )
 }
