1307962ec850fac86d758d1e9c9e5000b490e5a5
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-03-31 12:36:19 +0530

    CartProvider component created 2 function for removing and adding item from/to cart and then in app.js imported CartProvider and used as wrapper for app component

--- 07-FoodOrderApp/src/store/CartProvider.js
+++ 07-FoodOrderApp/src/store/CartProvider.js
@@ -2,8 +2,18 @@ import React from 'react';
 import CartContext from './cart-context';
 
 const CartProvider = (props) => {
+    const addItemToCartHandler=item=>{}
+    const removeItemFromCartHandler=id=>{}
+
+    const cartContext={
+        items:[],
+        totalAmount:0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
         {props.children}
     </CartContext.Provider>
   )
