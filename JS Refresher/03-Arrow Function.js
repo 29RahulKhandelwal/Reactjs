bec7eddeec680d90b2035cb28d19f65938d393fc
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-08-29 11:34:00 +0530

    Re-Starting

--- JS Refresher/03-Arrow Function.js
+++
@@ -1,9 +0,0 @@
-// function printMyName(name){
-//     console.log(name);
-// }
-// printMyName("Rahul");
-
-const printMyName=(name)=>{
-    console.log(name);
-}
-printMyName("Rahul");
--- JS Refresher/08-Destructoring.js
+++
@@ -1,8 +0,0 @@
-// const numbers=[1,2,3];
-// [n1,n2]=numbers;
-// console.log(n1);
-// console.log(n2);
-
-// [n1, ,n3]=numbers;
-// console.log(n1);
-// console.log(n3);
