17fd8a29be66c4d0d69deaa6e662bf5cdda94487
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-09-14 18:47:06 +0530

    Day1: Started Fresh with Expense Application

---
+++ 01-ExpenseTracker/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <>
+        <div>March 14th 2022</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$360.47</div>
+        </div>
+    </>
+  )
+}
+
+export default ExpenseItem
